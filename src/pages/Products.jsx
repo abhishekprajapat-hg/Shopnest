5f7869ddc2a94c4c1a614c2b2420df947013daae
Abhishek Prajapat <abhi03prajapat@gmail.com>
2025-07-05 22:22:08 +0530

    Add some more features

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -18,7 +18,7 @@ function Products() {
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">🛒 All Products</h2>
+      <h2 className="text-2xl font-bold mb-4 mt-4">🛒 All Products</h2>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
